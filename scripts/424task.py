6d3c9acbfacde217e881ef9d010ea0a6e192f2ea
Carrie <karina.andrsn@gmail.com>
2021-10-15 14:47:46 +0500

    task complete

--- scripts/424task.py
+++ scripts/424task.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
-import os
+import socket
+import time
 
+service_list = ['drive.google.com', 'mail.google.com', 'google.com']
+last_check_result = {}
+first_start = True
+
+
+def get_ip(service):
+    current_ip = socket.gethostbyname(service)
+    print(service, '-', current_ip)
+    if not first_start:
+        check_ip(service, current_ip)
+    last_check_result[service] = current_ip
+
+
+def check_ip(service, ip):
+    old_ip = last_check_result[service]
+    if ip != old_ip:
+        print('[ERROR] ' + service + ' IP mismatch: ' + old_ip + ' ' + ip)
+
+
+while 1 == 1:
+    for i in service_list:
+        get_ip(i)
+    first_start = False
+    print()
+    time.sleep(60)
