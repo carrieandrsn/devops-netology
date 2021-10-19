f55c287b771017dccc75580bd54bc427c7427a25
Carrie <karina.andrsn@gmail.com>
2021-10-19 16:00:21 +0500

    fix 4.33

--- scripts/433task.py
+++ scripts/433task.py
@@ -4,20 +4,20 @@ import sys
 import re
 import json
 import yaml
+import ast
 
 file_name = sys.argv[1]
 
 # this function will check file extensions based on first symbol
 # and will change is_json flag from initially assigned values
-
 def check_file_type(file_name, is_json):
     file = open(file_name, "r")
     first_symbol = file.read(1)
     file.close()
-    if (is_json and first_symbol != '{'):
+    if is_json and first_symbol != '{':
         print("File type mismatch. YAML file in .json \n")
         is_json = False
-    elif (not is_json and first_symbol == '{'):
+    elif not is_json and first_symbol == '{':
         print("File type mismatch. JSON file in .yml/.yaml \n")
         is_json = True
     else:
@@ -38,7 +38,7 @@ def json_syntax_check(file_name):
 def yaml_syntax_check(file_name):
     with open(file_name) as file:
         try:
-            yaml.load(file)
+            yaml.safe_load(file)
         except yaml.YAMLError as err:
             print(f"Invalid YAML: {err}")
             exit()
@@ -47,17 +47,25 @@ def yaml_syntax_check(file_name):
 
 
 def json2yaml(file_name):
-    new_name = file_name.replace(".json",".yml")
+    new_name = file_name.replace(".json", ".yml")
     in_file = open(file_name, "r")
-    text = in_file.read()
+    text = ast.literal_eval(in_file.read())
     in_file.close()
     out_file = open(new_name, "w+")
     yaml.dump(text, out_file)
     out_file.close()
+    print("Saved file " + new_name)
 
+def yaml2json(file_name):
+    new_name = file_name.replace(".yml", ".json").replace(".yaml", ".json")
+    in_file = open(file_name, "r")
+    text = yaml.safe_load(in_file)
+    in_file.close()
+    out_file = open(new_name, "w+")
+    json.dump(text, out_file)
+    out_file.close()
+    print("Saved file " + new_name)
 
-def yaml2json():
-    print("placeholder")
 
 # check extensions and exit if invalid
 if not bool(re.match(r".*\.(json|yml|yaml)$", file_name)):
@@ -68,7 +76,7 @@ elif bool(re.match(r".*\.(json)$", file_name)):
 else:
     is_json = False
 
-#does it look like contents are correct
+# does it look like contents are correct
 print("Checking if file type matches contents \n")
 check_file_type(file_name, is_json)
 
@@ -85,4 +93,3 @@ if is_json:
 else:
     print("Converting from YML to JSON \n")
     yaml2json(file_name)
-
